5d2bbf6729e15bcd86a8bef50654a930f7be0825
tonim <dquijote22@gmail.com>
2021-12-07 10:49:02 +0100

    fix issues with treesitter configurations, plugins needs to go before all langs and lsp configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,10 @@
 require("options")
+require("plugins")
+
 require("langs")
 require("lsp")
 require("keymappings")
 
-require("plugins")
 vim.g.colors_name = O.colorscheme -- Colorscheme must get called after plugins are loaded or it will break new installs.
 
 -- require("settings")
