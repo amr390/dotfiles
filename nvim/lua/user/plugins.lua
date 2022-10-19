0fecb22b0b0cc73a40bfabe0e75200e11a3296a4
amr <dquijote22@gmail.com>
2022-10-19 09:26:26 +0200

    restore project (need to check how it really works)

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -48,6 +48,7 @@ return packer.startup(function(use)
   use "kyazdani42/nvim-web-devicons"
   use "kyazdani42/nvim-tree.lua"
   use "akinsho/bufferline.nvim"
+  use "ahmedkhalf/project.nvim"
   use "moll/vim-bbye"
   use "nvim-lualine/lualine.nvim"
   use "akinsho/toggleterm.nvim"
