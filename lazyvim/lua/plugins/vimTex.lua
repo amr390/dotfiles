5e3ab8d7fb5df8c41eec0ae50fe76cdc066e819a
amr <dquijote22@gmail.com>
2024-04-03 10:00:43 +0200

    Fix vimtex

--- lazyvim/lua/plugins/vimTex.lua
+++ lazyvim/lua/plugins/vimTex.lua
@@ -6,13 +6,15 @@
 -- * override the configuration of LazyVim plugins
 return {
   -- add vimtex a laTex vim plugin
-  { "lervag/vimtex" },
-  init = function()
-    vim.g.vimtex_view_method = "zathura"
-    vim.o.foldmethod = "expr"
-    vim.o.foldexpr = "vimtex#fold#level(v:lnum)"
-    vim.o.foldtext = "vimtex#fold#text()"
-    -- To see at least the content of the sections upon opening
-    vim.o.foldlevel = 2
-  end,
+  {
+    "lervag/vimtex",
+    init = function()
+      vim.g.vimtex_view_method = "zathura"
+      vim.o.foldmethod = "expr"
+      vim.o.foldexpr = "vimtex#fold#level(v:lnum)"
+      vim.o.foldtext = "vimtex#fold#text()"
+      -- To see at least the content of the sections upon opening
+      vim.o.foldlevel = 2
+    end,
+  },
 }
