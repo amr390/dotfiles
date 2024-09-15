f4a8ac3c816faf1aff7c5805cd6f129b3fa2118d
amr <a.mas.rodriguez@gmail.com>
2024-09-15 21:05:04 +0200

    Reinstalled lazyvim from scratch

--- lazyvim/lua/config/keymaps.lua
+++ lazyvim/lua/config/keymaps.lua
@@ -1,6 +1,6 @@
--- maps are automatically loaded on the VeryLazy event
--- Default maps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/maps.lua
--- Add any additional maps here
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
 --
 -- This file is automatically loaded by lazyvim.config.init
 local Util = require("lazyvim.util")
---
+++ lazyvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    opts = {
+      transparent = true,
+    },
+  },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized-osaka",
+    },
+  },
+}
