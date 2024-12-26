4a37f0cd66b8b69252715611c28551863d96b757
amr <a.mas.rodriguez@gmail.com>
2024-12-26 17:41:53 +0100

    lazy config simplified

--- lazyvim/init.lua
+++ lazyvim/init.lua
@@ -1,12 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 -- vim.uv = vim.uv or vim.loop
---
+
 -- local M = {}
---
--- ---@param opts? LazyVimConfig
+
+---@param opts? LazyVimConfig
 -- function M.setup(opts)
 --   require("config").setup(opts)
 -- end
---
+
 -- return M
 require("config.lazy")
