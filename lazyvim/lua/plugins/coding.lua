2e7bb7eea892f2fa5016bb12734021daf651ab9c
amr <dquijote22@gmail.com>
2025-04-28 11:24:28 +0200

    try avante

--- lazyvim/lua/plugins/coding.lua
+++ lazyvim/lua/plugins/coding.lua
@@ -18,28 +18,21 @@ return {
       { "<leader>cV", "<cmd>VenvSelectCached<cr>" },
     },
   },
-  {
-    "kiddos/gemini.nvim",
-    config = function()
-      require("gemini").setup()
-    end,
-  },
+  -- {
+  --   "kiddos/gemini.nvim",
+  --   config = function()
+  --     require("gemini").setup()
+  --   end,
+  -- },
   {
     "yetone/avante.nvim",
+    -- enabled = false,
     event = "VeryLazy",
     version = false, -- Never set this value to "*"! Never!
     opts = {
       -- add any opts here
       -- for example
-      provider = "openai",
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-        timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-        temperature = 0,
-        max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-        --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-      },
+      provider = "gemini",
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = "make",
@@ -51,11 +44,11 @@ return {
       "MunifTanjim/nui.nvim",
       --- The below dependencies are optional,
       "echasnovski/mini.pick", -- for file_selector provider mini.pick
-      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      -- "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      -- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
       "ibhagwan/fzf-lua", -- for file_selector provider fzf
       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      -- "zbirenbaum/copilot.lua", -- for providers='copilot'
       {
         -- support for image pasting
         "HakonHarnes/img-clip.nvim",
