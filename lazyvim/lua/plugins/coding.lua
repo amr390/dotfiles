ffa233490d8745cd32ad196b54229b7cd4c5ab80
amr <dquijote22@gmail.com>
2025-05-08 17:32:03 +0200

    Lazy, mason rollback to v1

--- lazyvim/lua/plugins/coding.lua
+++ lazyvim/lua/plugins/coding.lua
@@ -76,4 +76,25 @@ return {
       },
     },
   },
+  {
+    "mhartington/formatter.nvim",
+    config = function()
+      require("formatter").setup({
+        logging = false,
+        filetype = {
+          python = {
+            -- Add formatters for Python files
+          },
+          html = {
+            -- Add formatters for HTML files
+            require("formatter.filetypes.html").prettier(),
+          },
+          django = {
+            -- Add formatters for Django templates
+            require("formatter.filetypes.html").prettier(),
+          },
+        },
+      })
+    end,
+  },
 }
